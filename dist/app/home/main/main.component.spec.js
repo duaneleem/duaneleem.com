25ca1af50a0ec669533d5200bf617dbb15767001
Duane Leem <duaneleem@alumni.ggu.edu>
2016-09-01 08:24:46 -0700

    Initial dist subtree commit

---
+++ dist/app/home/main/main.component.spec.js
@@ -0,0 +1,10 @@
+/* tslint:disable:no-unused-variable */
+"use strict";
+var main_component_1 = require('./main.component');
+describe('Component: Main', function () {
+    it('should create an instance', function () {
+        var component = new main_component_1.MainComponent();
+        expect(component).toBeTruthy();
+    });
+});
+//# sourceMappingURL=main.component.spec.js.map
---
+++ dist/app/home/summary/summary.component.spec.js
@@ -0,0 +1,10 @@
+/* tslint:disable:no-unused-variable */
+"use strict";
+var summary_component_1 = require('./summary.component');
+describe('Component: Summary', function () {
+    it('should create an instance', function () {
+        var component = new summary_component_1.SummaryComponent();
+        expect(component).toBeTruthy();
+    });
+});
+//# sourceMappingURL=summary.component.spec.js.map

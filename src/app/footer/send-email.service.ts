db13f767ff85874286ba81b6fd564e1d4f1e2d35
Duane Leem <duaneleem@alumni.ggu.edu>
2016-11-21 22:34:45 -0800

    Updated send-email service.

--- src/app/footer/send-email.service.ts
+++ src/app/footer/send-email.service.ts
@@ -3,9 +3,9 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class SendEmailService {
     objSenderInfo = {
-        strName: "",
-        strEmail: "",
-        strMessage: "",
+        strName: "Duane Leem",
+        strEmail: "duaneleem@alumni.ggu.edu",
+        strMessage: "Just a test message.",
         captchaResponse: ""
     }; // objSenderInfo
 }
